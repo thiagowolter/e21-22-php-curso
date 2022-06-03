f41023b034ab9d133f0e2c84599b42d84b60ccc6
thiagowolter <thiagowolter7@gmail.com>
2022-06-03 17:25:58 -0300

    0306

--- exercicios/php/ephp_021.php
+++ exercicios/php/ephp_021.php
@@ -2,3 +2,23 @@
 
 <!-- 21. Crie um programa que realiza a Progressão Aritmetica de 20 elementos, com primeiro termo e razão definidos pelo usuário.  (Progressão aritmética - Uma progressão aritmética é uma sequência numérica em que cada termo, a partir do segundo, é igual à soma do termo anterior com uma constante r. O número r é chamado de razão ou diferença comum da progressão aritmética.)  -->
 
+<form action="" method="POST">
+    Termo: <input type="number" name="termo">
+    Razao: <input type="number" name="razao">
+    <input type="submit" name="enviar">
+</form>
+
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $a = $_POST['termo'];
+    $r = $_POST['razao'];
+    for ($i = 1; $i <= 20; $i++) {
+        $a = $a + $r;
+        echo $a . "\n";
+        
+    }
+}
+
+?>
--- exercicios/php/php-video-aulas-1.11/aula_001.11.5.php
+++ exercicios/php/php-video-aulas-1.11/aula_001.11.5.php
@@ -5,23 +5,23 @@ lambda funcions no PHP são a Instancia de uma classe Closure -->
 
 <?php
 $remove_acento = function($str){
-    $a = array('ç','Ç');
-    $b = array('c','C');
+    $a = array('ç','Ç','ã','Ã');
+    $b = array('c','C','a','A');
     return str_replace($a,$b,$str);
 };
-echo $remove_acento('açafrao');
+echo $remove_acento('açafrão');
 echo  '<br>';
 
 ?>
 
 <!-- Inserir na array, todos os acentos e seus substitutivos sem acento  -->
 <?php
-$palavras = array();
-$palavras[] = 'conceiÇão';
-$palavras[] = 'cação';
-$palavras[] = 'caução';
-$r = array_map( $remove_acento,$palavras);
-print_r($r);
+$palavras = array('conceiÇão','cação','caução');
+foreach($palavras as $plv){
+    echo '<br>' ,$remove_acento($plv);
+}
+
+
 
 ?>
 
