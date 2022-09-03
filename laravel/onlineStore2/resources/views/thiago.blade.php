5b4c1e77be8091e82ec076fa78a78da4573c695a
Thiago Wolter <thiagowolter7@gmail.com>
2022-09-03 13:40:17 -0300

    home

--- laravel/onlineStore2/resources/views/thiago.blade.php
+++ laravel/onlineStore2/resources/views/thiago.blade.php
@@ -2,44 +2,33 @@
     @include('header')
   
   <body>
-  <div id="carouselExampleCaptions" class="carousel slide w-75" data-bs-ride="carousel">
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://i0.statig.com.br/bancodeimagens/bx/ry/fv/bxryfvt3vi76x0obfhixvrj8x.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+      <img src="https://uploaddeimagens.com.br/images/004/009/309/original/dortmund-22-23-kit-1.jpg?1662220310" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://uploads.metropoles.com/wp-content/uploads/2022/07/04104339/saiba_quais_sao_as_10_racas_de_cachorro_mais_amadas_no_brasil_widexl-600x400.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
+      <img src="https://uploaddeimagens.com.br/images/004/009/311/original/psg-nike-2022-2023-camiseta-qwr.jpg?1662220353" class="d-block w-100" alt="">
     </div>
     <div class="carousel-item">
-      <img src="https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png?quality=90&strip=info&resize=850,567" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
+      <img src="https://uploaddeimagens.com.br/images/004/009/312/original/2022-23-home-kit-player-gallery-05052022-cover.jpg?1662220382" class="d-block w-100" alt="...">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button  class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+  
     
     <!-- Optional JavaScript; choose one of the two! -->
 
